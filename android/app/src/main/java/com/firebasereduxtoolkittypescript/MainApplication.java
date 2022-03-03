4bb4dacc06f33dbbea7712636d10af32733d98aa
JulianErnest <julianernestcamello@gmail.com>
2022-03-03 17:06:57 +0800

    android prod build

--- android/app/src/main/java/com/firebasereduxtoolkittypescript/MainApplication.java
+++ android/app/src/main/java/com/firebasereduxtoolkittypescript/MainApplication.java
@@ -13,8 +13,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage; 
 import com.swmansion.reanimated.ReanimatedJSIModulePackage; 
+import androidx.multidex.MultiDexApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
