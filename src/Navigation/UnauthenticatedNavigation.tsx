6ecb77ccb44716f7793e5011b8aa863daeac20c3
Julian Ernest Camello <ernest@Julians-MacBook-Air.local>
2022-03-14 12:41:29 +0800

    added forgot password screen to unauthenticated stack

--- src/Navigation/UnauthenticatedNavigation.tsx
+++ src/Navigation/UnauthenticatedNavigation.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 
 import Confirm from '../Screens/Confirm';
+import ForgotPassword from '../Screens/ForgotPassword';
 import Login from '../Screens/Login';
 import Register from '../Screens/Register';
 
@@ -13,6 +14,7 @@ const UnauthenticatedNavigation = () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="Confirm" component={Confirm} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
     </Stack.Navigator>
   );
 };
