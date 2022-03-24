dfceaab1bf16f4b860c643416a0e23d9e20b3629
JulianErnest <76834365+JulianErnest@users.noreply.github.com>
2022-03-24 15:27:58 +0800

    Added retrieve CSV and validation to retrieve or not

--- src/Screens/Splashscreen.tsx
+++ src/Screens/Splashscreen.tsx
@@ -8,12 +8,24 @@ import {
   getLoggedInUser,
   setFetchingUser,
 } from '../State/Features/me/meSlice';
+import {
+  getPlayerCSV,
+  retrievePlayersFromStorage,
+  shouldGetPlayersCSV,
+} from '../State/Features/players/playersSlice';
 
 const Splashscreen = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
     (async () => {
       try {
+        const shouldGetVal = await shouldGetPlayersCSV();
+        if (shouldGetVal) {
+          dispatch(getPlayerCSV());
+        } else {
+          // Loads player list from local storage to memory
+          dispatch(retrievePlayersFromStorage());
+        }
         const uid = auth().currentUser?.uid ?? '';
         if (uid && uid !== 'dZf0mHnlbAPP1nYENilCmPW0U2C3') {
           await dispatch(getLoggedInUser(uid));
